8361a095505b608e3f20f5c555eb84ba04bc45bb
Albin Vass <albin.vass@gmail.com>
2020-10-25 01:31:14 +0200

    unfuck gitfuck

--- backend/src/lobby/session.ts
+++ backend/src/lobby/session.ts
@@ -165,7 +165,7 @@ let uniquePlayerName = (original_name, players) => {
 let joinLobby = (json, redis, rsub, rpub, log) => {
     log.debug(json);
     if(json.name) {
-        let session_id = getSessionIdFromLobbyId(json.join_code, redis)
+        let session_id = getSessionIdFromLobbyId(json.joinCode, redis)
         if(!session_id) {
             return {'error': "NO_SUCH_LOBBY"}
         }
