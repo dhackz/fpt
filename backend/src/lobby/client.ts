a9a60cd4875407df215d9d58560f05557c3dba43
dawidstrom <dawid.strom@gmail.com>
2020-10-24 16:33:52 +0200

    Add basic RedisHandler for handleJoinLobby.

--- backend/src/lobby/client.ts
+++ backend/src/lobby/client.ts
@@ -1,6 +1,6 @@
 import * as WebSocket from 'ws';
 
-const ws = new WebSocket('ws://localhost:8081')
+const ws = new WebSocket('ws://localhost:8080')
 
 ws.on('open', () => {
     console.log("Connection to websocket server established.")
