9660c4c088260ee7d638c9ac4d18392e9f66ee6d
Albin Vass <albin.vass@gmail.com>
2020-10-25 13:31:19 +0100

    Add cypress tests

--- frontend/src/Pages/HostOrJoin/index.tsx
+++ frontend/src/Pages/HostOrJoin/index.tsx
@@ -2,17 +2,30 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 import * as Styled from './styles';
 
+const HostButton = (props: {name: string}) => {
+  const history = useHistory();
+  return (
+    <div>
+      <Styled.BubblyBtn onClick={() => history.push('/create-lobby')}>Host game</Styled.BubblyBtn>
+    </div>
+  )
+}
+const JoinButton = (props: {name: string}) => {
+  const history = useHistory();
+  return (
+    <div>
+      <Styled.BubblyBtn onClick={() => history.push('/join-lobby')}>Join game</Styled.BubblyBtn>
+    </div>
+  )
+}
+
 const HostOrJoin = () => {
   const history = useHistory();
   return (
     <>
       <div style={{ padding: '3rem' }}>Welcome boiii</div>
-      <div>
-        <Styled.BubblyBtn onClick={() => history.push('/create-lobby')}>Host game</Styled.BubblyBtn>
-      </div>
-      <div>
-        <Styled.BubblyBtn onClick={() => history.push('/join-lobby')}>Join game</Styled.BubblyBtn>
-      </div>
+      <HostButton name={"hostbutton"}/>
+      <JoinButton name={"joinbutton"}/>
     </>
   );
 };
